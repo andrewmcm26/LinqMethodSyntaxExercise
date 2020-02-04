5eafab445d0d48c57f269de1ab84b94e651e0e45
Andrew McMahon <andrewmcm26@gmail.com>
2020-02-04 10:37:16 -0600

    Initial Commit, completed exercise

---
+++ LinqMethodSyntaxExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+
+namespace LinqMethodSyntaxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>
+            {  "Madden", "Backyard Baseball", "Halo", "NCAA Football '08" };
+
+            var orderedGames = videoGames.OrderBy(game => game.Length);
+            foreach(var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
+
+        }
+    }
+}
